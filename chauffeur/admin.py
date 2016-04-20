eac47320693a1b28e80f99dab093fc80b2372626
Omer Akram <om26er@gmail.com>
2016-04-21 00:03:43 +0500

    Finish user/driver login/registeration based on token authentication

--- chauffeur/admin.py
+++ chauffeur/admin.py
@@ -1,15 +1,70 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-from chauffeur.models import Driver, Customer
+from chauffeur.models import User, USER_TYPE_CUSTOMER, USER_TYPE_DRIVER
+
+
+class CustomerProxy(User):
+    class Meta:
+        verbose_name = 'Customer'
+        verbose_name_plural = 'Customers'
+        proxy = True
+
+
+class DriverProxy(User):
+    class Meta:
+        verbose_name = 'Driver'
+        verbose_name_plural = 'Drivers'
+        proxy = True
+
+
+class PanelAdminProxy(User):
+    class Meta:
+        verbose_name = 'Admin'
+        verbose_name_plural = 'Admins'
+        proxy = True
 
 
 class DriverAdmin(admin.ModelAdmin):
-    pass
+
+    fields = ('is_active', 'username', 'password', 'first_name', 'last_name',
+              'email', 'phone_number', 'photo', 'location',
+              'location_last_updated', 'driving_experience', 'number_of_hires',
+              'bio')
+
+    class Meta:
+        model = DriverProxy
+
+    def get_queryset(self, request):
+        return self.model.objects.filter(user_type=USER_TYPE_DRIVER)
 
 
 class CustomerAdmin(admin.ModelAdmin):
-    pass
+    fields = ('is_active', 'username', 'password', 'first_name', 'last_name',
+              'email', 'phone_number', 'photo', 'location', 'number_of_hires',
+              'vehicle_type', 'vehicle_category', 'vehicle_make',
+              'vehicle_model', 'initial_app_payment')
+
+    class Meta:
+        model = CustomerProxy
+
+    def get_queryset(self, request):
+        return self.model.objects.filter(user_type=USER_TYPE_CUSTOMER)
 
 
-admin.site.register(Driver, DriverAdmin)
-admin.site.register(Customer, CustomerAdmin)
+class PanelAdmin(admin.ModelAdmin):
+    fields = ('username', 'password', 'first_name', 'last_name', 'email',
+              'is_active', 'is_superuser', 'is_staff', 'last_login',
+              'date_joined', 'groups', 'user_permissions')
+
+    class Meta:
+        model = PanelAdminProxy
+
+    def get_queryset(self, request):
+        return self.model.objects.filter(is_staff=True)
+
+
+admin.site.register(CustomerProxy, CustomerAdmin)
+admin.site.register(DriverProxy, DriverAdmin)
+admin.site.register(PanelAdminProxy, PanelAdmin)
+admin.site.unregister(Group)
--- chauffeur/serializers.py
+++ chauffeur/serializers.py
@@ -1,26 +1,46 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
-from chauffeur.models import Customer, Driver
+from chauffeur.models import User
 
 
 class CustomerSerializer(serializers.ModelSerializer):
-
+    user_type = serializers.IntegerField(write_only=True)
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())])
     password = serializers.CharField(write_only=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    phone_number = serializers.CharField(required=True)
+    vehicle_type = serializers.CharField(required=True)
+    vehicle_make = serializers.CharField(required=True)
+    vehicle_model = serializers.CharField(required=True)
+    vehicle_category = serializers.CharField(required=True)
 
     class Meta:
-        model = Customer
-        fields = ('first_name', 'last_name', 'username', 'password', 'email',
-                  'phone_number', 'photo', 'location', 'number_of_hires',
-                  'vehicle_type', 'vehicle_make', 'vehicle_model',
-                  'vehicle_category', 'initial_app_payment', 'id')
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'password',
+                  'email', 'phone_number', 'photo', 'location',
+                  'number_of_hires', 'vehicle_type', 'vehicle_make',
+                  'vehicle_model', 'vehicle_category', 'initial_app_payment',
+                  'user_type')
 
 
 class DriverSerializer(serializers.ModelSerializer):
-
+    user_type = serializers.IntegerField(write_only=True)
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())])
     password = serializers.CharField(write_only=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    phone_number = serializers.CharField(required=True)
+    driving_experience = serializers.CharField(required=True)
 
     class Meta:
-        model = Driver
-        fields = ('first_name', 'last_name', 'username', 'email', 'password',
-                  'phone_number', 'driving_experience', 'photo', 'location',
-                  'location_last_updated', 'number_of_hires', 'bio', 'id')
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'password',
+                  'email', 'phone_number', 'photo', 'location',
+                  'location_last_updated', 'driving_experience',
+                  'number_of_hires', 'bio', 'user_type')
