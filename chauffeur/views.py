5d65adf531906e6e135cb3169a41b148c1086b8e
Omer Akram <om26er@gmail.com>
2016-05-11 16:02:36 +0500

    API to get user info aka /me

--- chauffeur/views.py
+++ chauffeur/views.py
@@ -231,3 +231,20 @@ class DriversAroundView(ListAPIView):
         if len(message) > 0:
             return Response(data=message, status=status.HTTP_400_BAD_REQUEST)
         return super().get(request, *args, **kwargs)
+
+
+class UserDetailsView(APIView):
+    permission_classes = (
+        custom_permissions.IsOwner, permissions.IsAuthenticated,
+    )
+
+    def get(self, request, **kwargs):
+        user = User.objects.get(id=self.request.user.id)
+        if user.user_type == USER_TYPE_CUSTOMER:
+            serializer = CustomerSerializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        elif user.user_type == USER_TYPE_DRIVER:
+            serializer = DriverSerializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
