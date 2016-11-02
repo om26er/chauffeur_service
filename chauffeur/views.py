e8822e671bec8871e67e6dedd2339f524d9dc054
Omer Akram <om26er@gmail.com>
2016-11-02 15:48:42 +0500

    common code

--- chauffeur/views.py
+++ chauffeur/views.py
@@ -460,24 +460,30 @@ class GetPrice(APIView):
 
 
 class PaytmView(APIView):
-    def get(self, *args, **kwargs):
+    def _get_request_data(self, method):
+        if method == 'GET':
+            data = self.request.query_params.lists()
+            print(data)
+            return data
+        else:
+            data = self.request.data.lists()
+            print(data)
+            return data
+
+    def _common(self, method):
         request_endpoint = self.request.path_info.split('/')[-1]
         if request_endpoint == 'generatechecksum.cgi':
             return HttpResponse(
-                generate(dict(self.request.query_params.lists())))
+                generate(dict(self._get_request_data(method))))
         elif request_endpoint == 'verifychecksum.cgi':
             return HttpResponse(
-                verify(dict(self.request.query_params.lists())))
+                verify(dict(self._get_request_data(method))))
         else:
             # Wont ever reach here
             return HttpResponse('unknown url')
 
+    def get(self, *args, **kwargs):
+        return self._common('GET')
+
     def post(self, *args, **kwargs):
-        request_endpoint = self.request.path_info.split('/')[-1]
-        if request_endpoint == 'generatechecksum.cgi':
-            return HttpResponse(generate(dict(self.request.data.lists())))
-        elif request_endpoint == 'verifychecksum.cgi':
-            return HttpResponse(verify(dict(self.request.data.lists())))
-        else:
-            # Wont ever reach here
-            return HttpResponse('unknown url')
+        return self._common('POST')
